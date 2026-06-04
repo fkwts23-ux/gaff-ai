30a74f96dad93479b07a7f13527686f3a2bb820d
fkwts23-ux <fkwts23@gmail.com>
2026-06-04 19:08:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,33 +1,33 @@
-import { watchFile, unwatchFile } from 'fs';
-import chalk from 'chalk';
-import { fileURLToPath } from 'url';
-import { Button, ButtonV2,Carousel, AIRich } from './lib/MessageBuilder.js';
+import { watchFile, unwatchFile } from  fs ;
+import chalk from  chalk ;
+import { fileURLToPath } from  url ;
+import { Button, ButtonV2,Carousel, AIRich } from  ./lib/MessageBuilder.js ;
 
 global.Button = Button;
 global.ButtonV2 = ButtonV2;
 global.Carousel = Carousel;
 global.AIRich = AIRich;
 
-global.pairingNumber = 201067023456;
+global.pairingNumber = 201270706561;
 global.owner = [
-['201067023456', 'EL KING', true],
-['', 'EL KING', true],
+[ 201270706561 ,  EL KING , true],
+[  ,  EL KING , true],
 ];
 
-global.namebot = 'EL KING';
-global.author = 'EL KING';
-global.source = 'https://chat.whatsapp.com/GE576u8Ff5A40vaQycC2IG';
+global.namebot =  EL KING ;
+global.author =  EL KING ;
+global.source =  https://chat.whatsapp.com/GE576u8Ff5A40vaQycC2IG ;
 
-global.wait = 'Loading... | جاري الانتظار';
-global.eror = 'There is an error... | وقع خطأ';
+global.wait =  Loading... | جاري الانتظار ;
+global.eror =  There is an error... | وقع خطأ ;
 
 global.pakasir = {
-slug: 'kilersbotz',
-apikey: 'bWDO2M8GcfruzXscdKNQJC3vw8Y8PV13',
+slug:  kilersbotz ,
+apikey:  bWDO2M8GcfruzXscdKNQJC3vw8Y8PV13 ,
 expired: 30, //1 = 1menit. 30 = 30menit
 };
 
-global.stickpack = 'Created By';
+global.stickpack =  Created By ;
 global.stickauth = namebot;
 
 global.multiplier = 38; // The higher, The harder levelup
@@ -37,43 +37,43 @@ global.rpg = {
 emoticon(string) {
 string = string.toLowerCase();
 let emot = {
-level: '📊',
-limit: '🎫',
-health: '❤️',
-stamina: '🔋',
-exp: '✨',
-money: '💹',
-bank: '🏦',
-potion: '🥤',
-diamond: '💎',
-common: '📦',
-uncommon: '🛍️',
-mythic: '🎁',
-legendary: '🗃️',
-superior: '💼',
-pet: '🔖',
-trash: '🗑',
-armor: '🥼',
-sword: '⚔️',
-pickaxe: '⛏️',
-fishingrod: '🎣',
-wood: '🪵',
-rock: '🪨',
-string: '🕸️',
-horse: '🐴',
-cat: '🐱',
-dog: '🐶',
-fox: '🦊',
-petFood: '🍖',
-iron: '⛓️',
-gold: '🪙',
-emerald: '❇️',
-upgrader: '🧰',
+level:  📊 ,
+limit:  🎫 ,
+health:  ❤️ ,
+stamina:  🔋 ,
+exp:  ✨ ,
+money:  💹 ,
+bank:  🏦 ,
+potion:  🥤 ,
+diamond:  💎 ,
+common:  📦 ,
+uncommon:  🛍️ ,
+mythic:  🎁 ,
+legendary:  🗃️ ,
+superior:  💼 ,
+pet:  🔖 ,
+trash:  🗑 ,
+armor:  🥼 ,
+sword:  ⚔️ ,
+pickaxe:  ⛏️ ,
+fishingrod:  🎣 ,
+wood:  🪵 ,
+rock:  🪨 ,
+string:  🕸️ ,
+horse:  🐴 ,
+cat:  🐱 ,
+dog:  🐶 ,
+fox:  🦊 ,
+petFood:  🍖 ,
+iron:  ⛓️ ,
+gold:  🪙 ,
+emerald:  ❇️ ,
+upgrader:  🧰 ,
 };
 let results = Object.keys(emot)
-.map((v) => [v, new RegExp(v, 'gi')])
+.map((v) => [v, new RegExp(v,  gi )])
 .filter((v) => v[1].test(string));
-if (!results.length) return '';
+if (!results.length) return   ;
 else return emot[results[0][0]];
 },
 };
@@ -81,6 +81,6 @@ else return emot[results[0][0]];
 let file = fileURLToPath(import.meta.url);
 watchFile(file, () => {
 unwatchFile(file);
-console.log(chalk.redBright("Update 'config.js'"));
+console.log(chalk.redBright("Update  config.js "));
 import("${file}?update=${Date.now()}");
 });
